7bea2e353426ff8055250ca8338dd8717f3913e2
HULLINGB <bradley.hullinger@ucdenver.edu>
2022-08-27 07:37:18 -0600

    Update PasswordCheckerAlgorithm.java

--- PasswordCheckerAlgorithm2.java
+++ PasswordCheckerAlgorithm2.java
@@ -1,5 +1,5 @@
 
-
+import java.util.Scanner;
 //This algorithm checks if the password entered contains 1 lowercase,
 //1 uppercase, and 1 special character, and that the password is at least 6 digits
 //and less than 21 digits
@@ -9,7 +9,14 @@ public class PasswordCheckerAlgorithm
 {
 	public static void main(String[] args) {
 	    //String password = "a@31Vv";
-        char[] array = password.toCharArray();
+        
+		
+	for( ; ; )
+	{
+		System.out.println("Please enter a password");
+		Scanner myObj = new Scanner(System.in);
+		String password = myObj.nextLine();
+		char[] array = password.toCharArray();
         char[] array2 = "abcdefghijklmnopqrstuvwxyz".toCharArray();
         char[] array3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
         char[] array4 = "!@#$%^&*()_+-=,?./".toCharArray();
@@ -21,77 +28,71 @@ public class PasswordCheckerAlgorithm
         int count = 0;
         int number = 1;
         int repeatCounter = 0;
-		
-		for( ; ; )
-		{
-			System.out.println("Please enter a password");
-			Scanner myObj = new Scanner(System.in);
-			String password = myObj.nextLine();
-			if(password.length() > 1 && password.length() < 21){
-				for(int i = 0; i < array2.length; i++){
-					for(int y = 0; y < array.length; y++){
-						if(array2[i] == array[y])
-						{
-						valid2 = true; 
-						}}}
-				for(int i = 0; i < array3.length; i++){
-					for(int e = 0; e < array.length; e++){
-						if(array3[i] == array[e]){
-						valid3 = true;
-						}}}
-				for(int i = 0; i < array4.length; i++){
-					for(int e = 0; e < array.length; e++){
-						if(array4[i] == array[e]){
-						valid4 = true;
-						}}}
-				/**
-			   for(int b = 0; b < array.length; b++){
-				   if(array.length > 1 && number < array.length){
-						if(array[b] == array[number]){
-							repeatCounter++;
-						}
-						if(array[b] != array[number]){
-							repeatCounter = 0;
-						}
-						if(repeatCounter > 1){
-							valid = false;
-						}}
-					number++; }}
-				**/
-			if(valid2 == true)
-			{
-				if(valid3 == true)
-				{
-					if(valid4 == true)
+		if(password.length() > 1 && password.length() < 21){
+			for(int i = 0; i < array2.length; i++){
+				for(int y = 0; y < array.length; y++){
+					if(array2[i] == array[y])
 					{
-						if(array.length > 5 && array.length < 21)
-						{
-							valid = true;
-						}
+					valid2 = true; 
+					}}}
+			for(int i = 0; i < array3.length; i++){
+				for(int e = 0; e < array.length; e++){
+					if(array3[i] == array[e]){
+					valid3 = true;
+					}}}
+			for(int i = 0; i < array4.length; i++){
+				for(int e = 0; e < array.length; e++){
+					if(array4[i] == array[e]){
+					valid4 = true;
+					}}}
+			/**
+			for(int b = 0; b < array.length; b++){
+				if(array.length > 1 && number < array.length){
+					if(array[b] == array[number]){
+						repeatCounter++;
 					}
-				}            
-			}
+					if(array[b] != array[number]){
+						repeatCounter = 0;
+					}
+					if(repeatCounter > 1){
+						valid = false;
+					}}
+				number++; }}
+				**/
+		if(valid2 == true)
+		{
+			if(valid3 == true)
+			{
+				if(valid4 == true)
+				{
+					if(array.length > 5 && array.length < 21)
+					{
+						valid = true;
+					}
+				}
+			}            
+		}
 			
-			if(valid2 == false)
-			{
-				valid = false;
-			}
-			if(valid3 == false)
-			{
-				valid = false;
-			}
-			if(valid4 == false)
-			{
-				valid = false;
-			}    
+		if(valid2 == false)
+		{
+			valid = false;
+		}
+		if(valid3 == false)
+		{
+			valid = false;
+		}
+		if(valid4 == false)
+		{
+			valid = false;
+		}    
 			
-			if(password.length() == 0){
-				System.out.println("No password has been entered, please enter a password");
-			}
-			if(password.length() > 20){
-				System.out.println("Your password is longer than 20 digits, make a shorter password");
-			}
-			  }
+		if(password.length() == 0){
+			System.out.println("No password has been entered, please enter a password");
+		}
+		if(password.length() > 20){
+			System.out.println("Your password is longer than 20 digits, make a shorter password");
+		}
+			  
 			if(valid == true)
 			{
 				System.out.println(valid);
@@ -105,6 +106,7 @@ public class PasswordCheckerAlgorithm
 			}
 			
 		}
+	}
 		
 		
 }
